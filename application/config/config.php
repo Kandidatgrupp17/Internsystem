477af077d359734f169fd1d02ee5c1b1004b4c4d
Oscar Carlsson <ocarlsson3@gmail.com>
2012-03-26 12:23:11 +0200

    blev el i config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/CHARM-dev';
+$config['base_url']	= 'http://localhost/Internsystem';
 
 /*
 |--------------------------------------------------------------------------
